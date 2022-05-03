702f2d867014e7c17731972b74145edae3112467
Ethan Hargus <ethanhargus@gmail.com>
2022-05-03 18:05:21 -0500

    Update vulkan_device.cpp

--- taichi/backends/vulkan/vulkan_device.cpp
+++ taichi/backends/vulkan/vulkan_device.cpp
@@ -2008,9 +2008,9 @@ void VulkanDevice::create_vma_allocator() {
       PFN_vkGetPhysicalDeviceMemoryProperties2KHR(vkGetInstanceProcAddr(
           volkGetLoadedInstance(), "vkGetPhysicalDeviceMemoryProperties2KHR"));
   vk_vma_functions.vkGetDeviceBufferMemoryRequirements =
-      table.vkGetDeviceBufferMemoryRequirementsKHR;
+      table.vkGetDeviceBufferMemoryRequirements2;
   vk_vma_functions.vkGetDeviceImageMemoryRequirements =
-      table.vkGetDeviceImageMemoryRequirementsKHR;
+      table.vkGetDeviceImageMemoryRequirements2;
 
   allocatorInfo.pVulkanFunctions = &vk_vma_functions;
 
